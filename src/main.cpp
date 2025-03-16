86ef1737d8b93c55280807b79c055ae62aac0de6
bgrhnzcn <bugrahanozcan16@gmail.com>
2025-03-17 00:58:28 +0300

    Starting to test sockets.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,19 @@
-
+#include <cstring>
 #include <iostream>
+#include <arpa/inet.h>
+#include <netinet/ip.h>
+#include <sys/socket.h>
+
+#define IP "127.0.0.1"
 
 int main(void)
 {
+	struct sockaddr_in int_sock;
+	memset(&int_sock, 0, sizeof(struct sockaddr_in));
+	int_sock.sin_family = AF_INET;
+	int_sock.sin_port = htons(6666);
+	inet_pton(AF_INET, IP, &int_sock.sin_addr);
+
 	std::cout << "Hello, World!" << std::endl;
+	return (0);
 }
